5011242823e327951e774eb837e03ad5abfb0767
Quentin CAUDRON <quentincaudron@gmail.com>
2020-01-17 13:08:04 -0800

    Comments for understanding

--- pawprint/statistics.py
+++ pawprint/statistics.py
@@ -12,14 +12,15 @@ class Statistics(object):
     """
 
     def __init__(self, tracker):
-
-        # Save the tracker
         self.tracker = tracker
 
     def __getitem__(self, tracker):
-        """Overload the [] operator."""
-
-        return Tracker(db=self.tracker.db, table="{}__{}".format(self.tracker.table, tracker))
+        """
+        Overload the [] operator. This is used to access the various statistics tables.
+        """
+        return Tracker(
+            db=self.tracker.db, table="{}__{}".format(self.tracker.table, tracker)
+        )
 
     def sessions(self, duration=30, clean=False, batch_size=100000):
         """Create a table of user sessions."""
@@ -27,17 +28,18 @@ class Statistics(object):
         # Create a tracker for basic interaction
         stats = self["sessions"]
 
-        # initialize Sessions container
+        # Initialize a Sessions container
         open_sessions = Sessions()
 
-        # If we're starting clean, delete the table
+        # If we're starting clean, drop the database table containint existing sessions
         if clean:
             stats.drop_table()
 
         # This try statement contains a few SQL queries; if any of them fail with a
         # ProgrammingError, the try is exited and the sessions will be rebuilt from scratch.
         try:
-            # try to get the last session; if the query fails, sessions built from scratch
+            # Try to get the time of the very last event stored in the database
+            # If the query fails, sessions will be built from scratch
             last_entry = pd.read_sql(
                 "SELECT last_timestamp FROM {} ORDER BY last_timestamp DESC LIMIT 1".format(
                     stats.table
@@ -45,11 +47,13 @@ class Statistics(object):
                 self.tracker.db,
             ).loc[0, "last_timestamp"]
 
-            # construct query for unique users since last data
+            # All events after this time are new to us, so construct the query
+            # to get the list of unique users since this last event
             user_query = "SELECT DISTINCT({}) FROM {} WHERE {} > %(last_entry)s".format(
-                self.tracker.user_field, self.tracker.table, self.tracker.timestamp_field
+                self.tracker.user_field,
+                self.tracker.table,
+                self.tracker.timestamp_field,
             )
-
             params = {"last_entry": str(last_entry)}
 
             # Get the unique users since the last data we've tracked
@@ -57,12 +61,16 @@ class Statistics(object):
                 self.tracker.user_field
             ].values
 
+            # If there are no users, exit because there's nothing to update
             if len(unique_users) == 0:
-                return  # if no users, exit because nothing to update
+                return
 
-            unique_user_str = "('" + "','".join([str(user) for user in unique_users]) + "')"
+            # Construct a PostgreSQL-friendly string representing the list of unique users
+            unique_user_str = (
+                "('" + "','".join([str(user) for user in unique_users]) + "')"
+            )
 
-            # grab last recorded session for each user in unique users
+            # Grab the last recorded session for each user in unique users
             last_sessions = pd.read_sql(
                 "SELECT DISTINCT ON ({}) * FROM {} WHERE {} IN {} ORDER BY {}, last_timestamp DESC".format(
                     self.tracker.user_field,
@@ -74,7 +82,7 @@ class Statistics(object):
                 self.tracker.db,
             )
 
-            # create UserCurrentSession() from last session of each unique user
+            # Create UserCurrentSession() from last session of each unique user
             for _, session in last_sessions.iterrows():
                 existing_session = UserCurrentSession(
                     user_id=session["user_id"],
@@ -82,16 +90,19 @@ class Statistics(object):
                     events=session["events"],
                     last_time=session["last_timestamp"],
                 )
-
                 open_sessions.current_sessions[session["user_id"]] = existing_session
 
-            # get user and timestamp values to user in DELETE query
-            rows_to_delete = zip(last_sessions["user_id"].values, last_sessions["timestamp"].values)
+            # We've fully reconstructed the last session of each user, so handle the case
+            # where the user's session was not finished when we last built Sessions
+            # Now we need to delete those sessions from the database so we don't double-count
+            rows_to_delete = zip(
+                last_sessions["user_id"].values, last_sessions["timestamp"].values
+            )
 
-            # delete last recorded sessions from table
+            # Delete the last recorded sessions from the sessions table
             try:
                 for user, time in rows_to_delete:
-                    time = pd.Timestamp(time)  # convert from numpy.datetime64 to pd.Timestamp
+                    time = pd.Timestamp(time)
                     delete_session_query = "DELETE FROM {} WHERE {} = '{}' AND {} = '{}'".format(
                         stats.table,
                         self.tracker.user_field,
@@ -107,31 +118,41 @@ class Statistics(object):
             last_entry = datetime(1900, 1, 1)
             params = {"last_entry": str(last_entry)}
 
-        # get first batch
+        # We have previous session information for all relevant users, if there is any
+        # We can now read all events since the last recorded event and construct sessions
+        # Let's grab the first batch of new events from the events table
         batch_number = 0
         events_query = "SELECT * FROM {} WHERE {} > %(last_entry)s LIMIT {} OFFSET {}".format(
-            self.tracker.table, self.tracker.timestamp_field, batch_size, batch_size * batch_number
+            self.tracker.table,
+            self.tracker.timestamp_field,
+            batch_size,
+            batch_size * batch_number,
         )
         events = pd.read_sql(events_query, self.tracker.db, params=params)
+
+        # While there are events, read them in batches and add them to user session data
         while events.shape[0] > 0:
+
             print("batch: ", batch_number)
             print("num events in batch: ", events.shape[0])
-            # process events
+
+            # Process events that we have read
             for _, event_row in events.iterrows():  # loop through events
-                open_sessions.add_event(  # add events to Sessions()
+                open_sessions.log_event(  # add events to Sessions()
                     user_id=event_row["user_id"],
                     timestamp=event_row["timestamp"],
                     events=event_row["event"],
                     duration=duration,
                 )
 
-            # write all (sorted) closed sessions to DB
-            # resets `open_sessions.closed_sessions` to empty list
-            # leaves `open_sessions.current_sessions` as-is for next run through loop
+            # Write all (sorted) closed sessions to database
+            # This resets `open_sessions.closed_sessions` to empty list
+            # and leaves `open_sessions.current_sessions` as-is for next run through loop
             open_sessions.write_to_db(
                 table=stats.table, db=self.tracker.db, if_exists="append", index=False
             )
-            # grab next batch
+
+            # Grab the next batch of events from the database
             batch_number += 1
             events = pd.read_sql(
                 "SELECT * FROM {} WHERE {} > %(last_entry)s LIMIT {} OFFSET {}".format(
@@ -144,7 +165,7 @@ class Statistics(object):
                 params=params,
             )
 
-        # close and write any leftover open sessions
+        # Close and write any leftover open sessions
         open_sessions.close_open_sessions()
         open_sessions.write_to_db(
             table=stats.table, db=self.tracker.db, if_exists="append", index=False
@@ -163,7 +184,9 @@ class Statistics(object):
         # Determine whether the stats table exists and contains data, or if we should create one
         try:  # if this passes, the table exists and may contain data
             last_entry = pd.read_sql(
-                "SELECT timestamp FROM {} ORDER BY timestamp DESC LIMIT 1".format(stats.table),
+                "SELECT timestamp FROM {} ORDER BY timestamp DESC LIMIT 1".format(
+                    stats.table
+                ),
                 self.tracker.db,
             ).loc[0, "timestamp"]
         except ProgrammingError:  # otherwise, the table doesn't exist
@@ -179,10 +202,14 @@ class Statistics(object):
         # If we're also calculating by imposing a minimum number of events per user
         if min_sessions:
             # Count the number of rows per user in the sessions table
-            session_counts = self["sessions"].read().groupby(self.tracker.user_field).count()
+            session_counts = (
+                self["sessions"].read().groupby(self.tracker.user_field).count()
+            )
 
             # Select the active users where there are at least min_sessions rows per user
-            active_users = session_counts[session_counts["duration"] >= min_sessions].index
+            active_users = session_counts[
+                session_counts["duration"] >= min_sessions
+            ].index
             active_users = [str(user) for user in active_users]
 
             # If there are no users that qualify, turn off min_sessions calculations
@@ -195,14 +222,18 @@ class Statistics(object):
         )
         if not len(stickiness):  # if this has been run too recently, do nothing
             return
-        stickiness.rename(columns={"count": "dau", "datetime": "timestamp"}, inplace=True)
+        stickiness.rename(
+            columns={"count": "dau", "datetime": "timestamp"}, inplace=True
+        )
         stickiness.index = pd.to_datetime(stickiness["timestamp"])
         stickiness.drop("timestamp", axis=1, inplace=True)
         stickiness = stickiness.resample("D").sum().fillna(0).astype(int)
 
         # Calculate DAU for active users if requested
         if min_sessions:
-            active_users_query = {"{}__in".format(self.tracker.user_field): list(active_users)}
+            active_users_query = {
+                "{}__in".format(self.tracker.user_field): list(active_users)
+            }
             active_dau = self["sessions"].count(
                 "DISTINCT({})".format(self.tracker.user_field),
                 timestamp__gt=start,
@@ -273,7 +304,9 @@ class Statistics(object):
         # Calculate engagement as DAU / MAU
         stickiness["engagement"] = stickiness.dau / stickiness.mau
         if min_sessions:
-            stickiness["engagement_active"] = stickiness.dau_active / stickiness.mau_active
+            stickiness["engagement_active"] = (
+                stickiness.dau_active / stickiness.mau_active
+            )
 
         # Active user counts should be ints
         stickiness.wau = stickiness.wau.astype(int)
