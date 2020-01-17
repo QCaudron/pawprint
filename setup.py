b9b1b28d57441eb931828f88196a064a85f99e18
Jacob Deppen <jacob.deppen@cbre.com>
2020-01-17 13:32:57 -0800

    bump to version 2.1.0 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pawprint",
-    version="2.0.0",
+    version="2.1.0",
     description="A flexible event tracker for rapid analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
